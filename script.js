756fa9ad2f0009cd6c85bac97f19119a6d751c0e
PalomaBayon <bayonpaloma@gmail.com>
2026-08-13 11:49:13 -0300

    ppner solo dos proyectos y agregar link orl

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const nav = document.querySelector("#js-nav");
 const abrir = document.querySelector("#js-abrir");
 const cerrar = document.querySelector("#js-cerrar");
@@ -12,18 +11,3 @@ cerrar.addEventListener("click", () => {
   nav.classList.remove("visible");
   document.body.classList.remove("no-scroll");
 });
-=======
-const nav = document.querySelector("#js-nav");
-const abrir = document.querySelector("#js-abrir");
-const cerrar = document.querySelector("#js-cerrar");
-
-abrir.addEventListener("click", () => {
-  nav.classList.add("visible");
-  document.body.classList.add("no-scroll");
-});
-
-cerrar.addEventListener("click", () => {
-  nav.classList.remove("visible");
-  document.body.classList.remove("no-scroll");
-});
->>>>>>> 69ec803fb260e19a1ab7ca13ced68ae1f88b03d7
